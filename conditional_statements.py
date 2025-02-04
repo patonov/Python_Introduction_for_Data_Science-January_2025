a4c6e619b3ba7aed3ad1c408c683bd853a3d2753
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-02-04 22:13:30 +0200

    Add files via upload

---
+++ conditional_statements.py
@@ -0,0 +1,20 @@
+# number = int(input())
+
+# is_positive = number >= 0
+
+# if is_positive == True :
+#     if number == 0:
+#         print("The number is zero")
+#     else:        
+#         print(f"{number} is positive. Yuhuuuu!")
+# else :
+#     print(f"{number} is negative. Let it snow, let it snow!")
+
+free_friend = input()
+
+if free_friend == "Pesho":
+    print("We will drink too much.")
+elif free_friend == "Gosho":
+    print("We will go driving bicycles.")
+else:
+    print("Watching movie is a better option.")
