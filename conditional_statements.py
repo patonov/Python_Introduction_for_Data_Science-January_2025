ef61b12d922d5c66bd32778a3b354a51e1bba275
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-02-08 22:11:14 +0200

    Add files via upload

--- conditional_statements.py
+++ conditional_statements.py
@@ -10,11 +10,29 @@
 # else :
 #     print(f"{number} is negative. Let it snow, let it snow!")
 
-free_friend = input()
+# free_friend = input()
 
-if free_friend == "Pesho":
-    print("We will drink too much.")
-elif free_friend == "Gosho":
-    print("We will go driving bicycles.")
+# if free_friend == "Pesho":
+#     print("We will drink too much.")
+# elif free_friend == "Gosho":
+#     print("We will go driving bicycles.")
+# else:
+#     print("Watching movie is a better option.")
+
+# first_num = 4.50
+# second_num = 5.50
+# print(f"{round(first_num):.3f} {round(second_num):.2f}")
+
+# number = int(input())
+# if number % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+ticket_type = input()
+if ticket_type == "regular":
+    print("$1.60")
+elif ticket_type == "student":
+    print("1.00")
 else:
-    print("Watching movie is a better option.")
+    print("Invalid ticket type!")
