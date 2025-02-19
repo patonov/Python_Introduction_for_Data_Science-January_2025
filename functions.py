ca06e0125744e4d8e2bd902a6bf7900d1350a624
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-02-19 22:34:41 +0200

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,7 +1,21 @@
-def text_printer(text):
-    for _ in range(0, 3):
-        print(text)
+# def text_printer(text):
+#     for _ in range(0, 3):
+#         print(text)
 
 
-text_printer("tralala tralala tralala")
+# text_printer("tralala tralala tralala")
+
+# def greet_beer_drinker(name: str, does_drink_beer: bool = True):
+#     if does_drink_beer:
+#         print("You are really great.")
+#     else:
+#         print(f"{name}, you are not my party.")
+
+# greet_beer_drinker("Gosho")
+# greet_beer_drinker("Pesho", False)
+
+def multiple_by_two(num: int) -> int:
+    return num * 2
+
+print(multiple_by_two(3))
 
