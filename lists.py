4bed5f746dcae0e8b69c9c1d943aaa281dcfcf44
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-02-20 23:11:24 +0200

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,20 @@
+# my_list = list()
+
+# my_string = input()
+
+# my_list = my_string.split(" ")
+
+# print(my_list[0])
+# print(len(my_list))
+
+# print("-".join(my_list))
+
+new_list = []
+last_list = [1, 2, 3, 4]
+
+# for element in last_list:
+#     new_list.append(str(element))
+
+# print(new_list)
+
+print([str(element) for element in last_list])
