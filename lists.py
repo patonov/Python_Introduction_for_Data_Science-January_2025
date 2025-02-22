f958075b89642f200f5a4c8c988bf09358e18c9f
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-02-22 19:50:47 +0200

    Add files via upload

--- lists.py
+++ lists.py
@@ -9,12 +9,49 @@
 
 # print("-".join(my_list))
 
-new_list = []
-last_list = [1, 2, 3, 4]
+# new_list = []
+# last_list = [1, 2, 3, 4]
 
 # for element in last_list:
 #     new_list.append(str(element))
 
 # print(new_list)
 
-print([str(element) for element in last_list])
+# print([str(element) for element in last_list])
+
+# my_string = "abcdefghijklmnopqrstuwxyz"
+# my_list = list(my_string)
+# print(my_list)
+
+vegetables = ["carrot", "cucumber", "pumpkin"]
+vegetables.append("tomato")
+vegetables.append("tomato")
+vegetables.append("tomato")
+vegetables.append("tomato")
+
+vegetables.remove("tomato")
+del vegetables[len(vegetables) - 1]
+
+# print(vegetables[-1])
+# print(vegetables.count("tomato"))
+
+# for index, value in enumerate(vegetables):
+#     print(f"{index} => {value}")
+
+# for index in range(len(vegetables) - 1, -1, -1):    
+#     print(f"Going to delete {vegetables[index]}")
+#     vegetables.remove(vegetables[index])
+#     print(vegetables)
+
+while "tomato" in vegetables:
+    vegetables.remove("tomato")
+
+vegetables.sort(reverse=True)
+print(vegetables)
+print(vegetables.pop())
+
+vegetables.insert(1, "potato")
+vegetables[::-1] #vegetables.reverse()
+print(vegetables)
+
+
