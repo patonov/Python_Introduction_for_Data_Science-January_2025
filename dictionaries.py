8ba0d39e2442b41e66749387a769a00896a35465
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-03-03 22:39:35 +0200

    Add files via upload

--- dictionaries.py
+++ dictionaries.py
@@ -12,7 +12,16 @@ age_dictionary = {"ivan": 16, "pencho": 19, "mitko": 21, "dimitrichko": 18}
 
 # print(age_dictionary)
 
-name = input().lower()
+# name = input().lower()
 
-if name in age_dictionary.keys():
-    print(age_dictionary[name])
+# if name in age_dictionary.keys():
+#     print(age_dictionary[name])
+
+children = {"first_child": "Ivancho", "toys": ["monkey", "dragon", "ball"]}
+toys = {"plastic": {"animals": ["monkey", "dragon", "rabit"]}, "rubber": {"balls": ["football", "tenis ball"]}}
+
+print(toys["plastic"]["animals"][1])
+
+
+
+
