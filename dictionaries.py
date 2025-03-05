27c52914d947e3726a74f5c2f0c0f47ab21cddf7
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-03-05 22:36:00 +0200

    Add files via upload

--- dictionaries.py
+++ dictionaries.py
@@ -17,11 +17,26 @@ age_dictionary = {"ivan": 16, "pencho": 19, "mitko": 21, "dimitrichko": 18}
 # if name in age_dictionary.keys():
 #     print(age_dictionary[name])
 
-children = {"first_child": "Ivancho", "toys": ["monkey", "dragon", "ball"]}
-toys = {"plastic": {"animals": ["monkey", "dragon", "rabit"]}, "rubber": {"balls": ["football", "tenis ball"]}}
+# children = {"first_child": "Ivancho", "toys": ["monkey", "dragon", "ball"]}
+# toys = {"plastic": {"animals": ["monkey", "dragon", "rabit"]}, "rubber": {"balls": ["football", "tenis ball"]}}
 
-print(toys["plastic"]["animals"][1])
+# print(toys["plastic"]["animals"][1])
 
+# copy_toys = toys.copy()
+# print(f"The copy of the toys dictionary is: {copy_toys}")
 
+# copy_toys.clear()
+# print(copy_toys)
 
+# print(toys.pop("plastic"))
+# print(toys.popitem())
 
+# del children["toys"]  
+# print(children)
+
+children_names = ["gogi", "dimitrichko", "mimi"]
+children_ages = [18, 11, 17]
+
+#children_data = dict(zip(children_names, children_ages))
+children_data = {name: age for (name, age) in zip(children_names, children_ages)}
+print(children_data)
